3541a9263c71db72ba51d616149ded80ea0a5240
Savin Alexander Viktorovich <alexo.veto@gmail.com>
2022-02-08 16:13:37 +0300

    fix base href

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -102,8 +102,8 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "a9467ed179cef35273de691558bf7658",
-"/": "a9467ed179cef35273de691558bf7658",
+"index.html": "d8e4d30fe93436d14aba44534c587a77",
+"/": "d8e4d30fe93436d14aba44534c587a77",
 "main.dart.js": "5546e841d0011ce13245db67e285b3c1",
 "manifest.json": "d8f04622558c0880fac4d3f17ed59489",
 "version.json": "93b6982f08f759c1bb68040ddecb417d"
