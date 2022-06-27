15f65357993f66c16352789b99e376dae0daedd8
Alexqwesa <alexo.veto@gmail.com>
2022-06-27 00:29:30 +0000

    Deploy ais3uson to web

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -8,8 +8,8 @@ const RESOURCES = {
 "canvaskit/profiling/canvaskit.js": "ae2949af4efc61d28a4a80fffa1db900",
 "canvaskit/profiling/canvaskit.wasm": "95e736ab31147d1b2c7b25f11d4c32cd",
 "canvaskit/canvaskit.wasm": "4b83d89d9fecbea8ca46f2f760c5a9ba",
-"index.html": "a01b66faa56cc2f09f2849cc8b0f11c8",
-"/": "a01b66faa56cc2f09f2849cc8b0f11c8",
+"index.html": "0463ed04b8d6e89205ddeae444b5681d",
+"/": "0463ed04b8d6e89205ddeae444b5681d",
 "flutter.js": "0816e65a103ba8ba51b174eeeeb2cb67",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
@@ -179,7 +179,7 @@ const RESOURCES = {
 "assets/packages/record_windows/assets/fmedia/mod/aac.dll": "533a3cbd100c91c23861effeb3311cef",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
 "assets/NOTICES": "b2f19a00ee6b2c326078132ae596a8cb",
-"main.dart.js": "46246c6c467244a2077d00a5902af974"
+"main.dart.js": "4fe3d0738cba4587f0a61e725618a654"
 };
 
 // The application shell files that are downloaded before a service worker can
